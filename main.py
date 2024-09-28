95aeb8133db6a19649ea71e8b4beae3fb50d34c6
sm7177 <subrinamaharjan2020@gmail.com>
2024-09-28 15:53:37 +0545

    lists

---
+++ main.py
@@ -0,0 +1,60 @@
+# Activity-1
+# list=["apple","mango","banana","orange"]
+# print("length of list:",len(list))
+
+# print("first element:", list[0])
+
+# print("last element:", list[-1])
+
+# list.append("grape")
+# print("updated list:", list)
+
+# list.remove("mango")
+# print("updated list after removing 'mango':", list)
+
+# list.pop(1)
+# print(list)
+
+# list.reverse()
+# print("reversed list:", list)
+
+# list.sort()
+# print("sorted list:", list)
+
+# list=list[0:2]
+# print(list)
+
+# list.clear()
+# print(list)
+
+
+
+
+# Activity-2
+# dict = {"name":"Subrina","age":10,1:[1,2,3,4,5,6,7,8,9]}
+# print(dict["name"])
+# print(dict[1][3])
+
+# dict["age"]=20
+
+# dict["address"]="new road"
+# print(dict)
+
+# dict.pop(1)
+# print(dict)
+
+# dict.clear()
+# print(dict)
+
+
+
+
+#Activity-3
+def test(list):
+    result={}
+    for item in list:
+        result[item[0]]=item[1:]
+    return result
+students=[[1,"ram",19],[2,"shyam",18],[3,"sita",19],[4,"ruby",17],[5,"red",20]]
+print(students)
+print(test(students))
